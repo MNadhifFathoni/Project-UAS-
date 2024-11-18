256e97cad04bbe5a6e5c3c2998d1a9994df4a3d2
Muhammad Nadhif Fathoni <24031554141@mhs.unesa.ac.id>
2024-11-18 18:32:04 +0700

    Create default starter main.py project
    Hal yang telah dilakukan
    1. Import tkinter dan customtkinter
    2. Size  (geometry)
    3. title

---
+++ main_project.py
@@ -0,0 +1,15 @@
+import tkinter
+import customtkinter
+
+# start
+app = customtkinter.CTk()
+
+# header
+customtkinter.set_appearance_mode("Dark")
+customtkinter.set_default_color_theme("green")
+app.geometry("1024x512")
+app.title("Credit Card Custom")
+
+
+
+app.mainloop()
